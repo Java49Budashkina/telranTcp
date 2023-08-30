e374cb6c21f8557e4ed60bf7807d95e00f1bfa18
Anna <annbudash@mail.ru>
2023-08-30 19:22:09 +0300

    did request with double[]

--- input-outputnetwork/src/telran/net/CalculatorProtocol.java
+++ input-outputnetwork/src/telran/net/CalculatorProtocol.java
@@ -6,14 +6,14 @@ public class CalculatorProtocol implements ApplProtocol {
 	public Response getResponse(Request request) {
 		ResponseCode code = ResponseCode.WRONG_DATA;
 		String res = "Wrong request structure, usage: <request operator>#<operand1>#<operand2>";
-		String [] tokens = request.requestData().toString().split("#");
+		Double[] tokens = (Double[]) request.requestData();
 		if(tokens.length == 2) {
 			code = ResponseCode.OK;
 			res = switch(request.requestType()) {
-			case "+" -> Double.toString( Double.valueOf( tokens[0]) + Double.valueOf(tokens[1])) ;
-			case "-" -> Double.toString( Double.valueOf( tokens[0]) - Double.valueOf(tokens[1])) ;
-			case "*" -> Double.toString( Double.valueOf( tokens[0]) * Double.valueOf(tokens[1])) ;
-			case "/" -> Double.toString( Double.valueOf( tokens[0]) / Double.valueOf(tokens[1])) ;
+			case "+" -> Double.toString( tokens[0] + tokens[1]) ;
+			case "-" -> Double.toString( tokens[0] - tokens[1]) ;
+			case "*" -> Double.toString( tokens[0] * tokens[1]) ;
+			case "/" -> Double.toString( tokens[0] / tokens[1]) ;
 			default -> { code = ResponseCode.WRONG_TYPE;
 					yield "wrong request type"; }
 			};
--- input-outputnetwork/src/telran/net/CalculatorTcpClient.java
+++ input-outputnetwork/src/telran/net/CalculatorTcpClient.java
@@ -7,27 +7,25 @@ import telran.view.*;
 public class CalculatorTcpClient {
 
 	public static void main(String[] args) throws Exception {
-		TcpHandler client = new TcpHandler("localhost", 5014);
-		
+		try(TcpHandler client = new TcpHandler("localhost", 5014)) {		
 		InputOutput io = new ConsoleInputOutput();
 		 Menu menu = new Menu("TCPcalculator", Item.of("send request", io1 -> {
 			 HashSet<String> requests = new HashSet<>(Arrays.asList("+", "-", "*", "/"));
 			 String oper = io1.readString("Enter arithmetic operator  " + requests, "", requests);
-			 String operand1 = io1.readString("Enter the first operand");
-			 String operand2 = io1.readString("Enter the second operand");
+			 Double[] op = new Double[2];
+			 
+			 op[0] = io1.readDouble("Enter the first operand", "");
+			 op[1] = io1.readDouble("Enter the second operand", "");
+			
 			 try {
-			 String response = client.send(oper, String.format("%s#%s",  operand1, operand2));
+			 String response = client.send(oper, op);
 			 io1.writeLine(response);
 			} catch (Exception e) {
 				throw new RuntimeException(e.toString());
 			}
 		 }), Item.ofExit());
 		 menu.perform(io);
-		client.close();
-		
-		
-		
-		
+		}
 		
 		
 	}
