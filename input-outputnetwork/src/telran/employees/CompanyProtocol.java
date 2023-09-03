434ce206cfc1d4254b8d1e4ca91542814110e42c
Anna <annbudash@mail.ru>
2023-09-03 11:42:40 +0300

    HW#42

--- input-outputnetwork/src/telran/employees/CompanyProtocol.java
+++ input-outputnetwork/src/telran/employees/CompanyProtocol.java
@@ -3,13 +3,15 @@ package telran.employees;
 import java.io.Serializable;
 import java.util.ArrayList;
 
-import telran.employees.dto.Employee;
-import telran.employees.dto.UpdateData;
+import telran.employees.controller.CompanyController;
+import telran.employees.dto.*;
+
 import telran.employees.service.Company;
 import telran.net.ApplProtocol;
 import telran.net.Request;
 import telran.net.Response;
 import telran.net.ResponseCode;
+import telran.view.Item;
 
 public class CompanyProtocol implements ApplProtocol {
 
@@ -27,11 +29,18 @@ public class CompanyProtocol implements ApplProtocol {
 		try {
 			Serializable responseData = switch(requestType) {
 			case "employee/add" -> employee_add(data);
+			case "employee/remove" -> employee_remove(data);
 			case "employee/get" -> employee_get(data);
 			case "employees/get" -> employees_get(data);
+			case "department/salary/distribution" -> department_salary_distribution(data);
+			case "salary/distribution" -> salary_distribution(data);
+			case "employees/department" -> employees_department(data);
+			case "employees/salary" -> employees_salary(data);
+			case "employees/age" -> employees_age(data);
+			case "salary/update" -> salary_update(data);
 			case "department/update" -> department_update(data);
-			//TODO add rest of the protocol methods
-			    default -> new Response(ResponseCode.WRONG_TYPE, requestType +
+			
+			default -> new Response(ResponseCode.WRONG_TYPE, requestType +
 			    		" is unsupported in the Company Protocol");
 			};
 			response = (responseData instanceof Response) ? 
@@ -44,7 +53,47 @@ public class CompanyProtocol implements ApplProtocol {
 		return response;
 	}
 
-	 private Serializable department_update(Serializable data) {
+	 private Serializable salary_update(Serializable data) {
+		 @SuppressWarnings("unchecked")
+			UpdateData<String> updateData = (UpdateData<String>) data;
+			long id = updateData.id();
+			int salary = Integer.parseInt(updateData.data());
+		return company.updateSalary(id, salary);
+	}
+
+	private Serializable employees_age(Serializable data) {
+		FromTo rang= (FromTo) data;
+		return new ArrayList<>( company.getEmployeesByAge(rang.from(), rang.to()));
+	}
+
+	private Serializable employees_salary(Serializable data) {
+		FromTo rang= (FromTo) data;
+		return new ArrayList<>( company.getEmployeesBySalary(rang.from(), rang.to()));
+
+	}
+
+	private Serializable employees_department(Serializable data) {
+		String dept = (String) data;
+		return new ArrayList<>( company.getEmployeesByDepartment(dept));
+
+	}
+
+	private Serializable salary_distribution(Serializable data) {
+		int salary = (int) data;
+		return new ArrayList<>( company.getSalaryDistribution(salary));
+
+	}
+
+	private Serializable department_salary_distribution(Serializable data) {
+		return new ArrayList<>( company.getDepartmentSalaryDistribution());
+	}
+
+	private Serializable employee_remove(Serializable data) {
+		int id = (int) data;
+		return company.removeEmployee(id);
+	}
+
+	private Serializable department_update(Serializable data) {
 		
 		@SuppressWarnings("unchecked")
 		UpdateData<String> updateData = (UpdateData<String>) data;
